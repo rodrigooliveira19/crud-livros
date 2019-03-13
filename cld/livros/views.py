906bce1fde6c646f77ecb56a025e2373ce073cc4
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-03-13 15:49:18 -0300

    Criação de views, urls e template

--- cld/livros/views.py
+++ cld/livros/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from . import urls
 
 # Create your views here.
+
+def index(request):
+	return HttpResponse("Olá, Mundo. Vocé está na página index de livros")
+
+def livro_list(request):
+	return render(request,'livros/livro_list.html',{})
+
