efff5423ec67d7df697f2f75e0fd556533b5e757
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-03-18 11:54:43 -0300

    Adicionando operações de de editar e excluir um livro

--- cld/livros/urls.py
+++ cld/livros/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('livro/<int:pk>/', views.livro_detail, name='livro_detail'),  
 	path('livro/new',views.livro_new,name='livro_new'),
 	path('livro/<int:pk>/edit/',views.livro_edit,name='livro_edit'), 
+	path('livro/<int:pk>/delete/', views.livro_delete, name='livro_delete'),
 ]
--- cld/livros/views.py
+++ cld/livros/views.py
@@ -21,8 +21,6 @@ def livro_new(request):
 		form = LivroForm(request.POST)
 		if form.is_valid():
 			livro = form.save(commit=False)
-			#post.author = request.user
-			#post.published_date = timezone.now()
 			livro.save()
 			return redirect('livro_detail',pk=livro.pk)#
 	else:
@@ -35,16 +33,26 @@ def livro_detail(request, pk):
 
 
 def livro_edit(request,pk):
-	livro =get_object_or_404(Livro,pk=pk)
+	livro = get_object_or_404(Livro,pk=pk) #Retorna o objeto Livro a editar.
 	if request.method == "POST":
 		form = LivroForm(request.POST,instance=livro)
-		if forms.is_valid():
+		if form.is_valid():
 			livro = form.save(commit=False)
 			livro.save()
 			return redirect('livro_detail',pk=livro.pk)
 	else:
 		form = LivroForm(instance=livro)
-	return render(request,'livros/livro_edit.html',{'form',form})
+	return render(request,'livros/livro_edit.html',{'form':form})
+
+
+def livro_delete(request,pk):
+	livro = get_object_or_404(Livro,pk=pk)
+	if request.method =="POST":
+		livro.delete()
+		return redirect('livro_list')
+	else:
+		form = LivroForm(instance=livro)
+	return render(request,'livros/livro_delete.html',{'livro':livro})
 
 
 
