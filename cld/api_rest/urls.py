8f0e1a846523044e22467bb0c9a94c4f3eeaddc3
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-03-18 15:16:23 -0300

    Criação de web service para cadastrar livro

--- cld/api_rest/urls.py
+++ cld/api_rest/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('livros/',views.LivroListServiceView.as_view(),name='livros'),
+    path('cadastrarLivro/',views.CadastrarLivroServiceView.as_view(),name='cadastrarLivro'), 
 ]
--- cld/api_rest/views.py
+++ cld/api_rest/views.py
@@ -3,10 +3,33 @@ from django.shortcuts import render
 from livros import models
 from api_rest import serializers
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+
 
 # Create your views here.
 
 class LivroListServiceView(generics.ListCreateAPIView):
 	queryset = models.Livro.objects.all()
 	serializer_class = serializers.LivroSerializer 
+
+
+class CadastrarLivroServiceView(APIView):
+
+	def post(self,request,format=None):
+
+		codigo = request.data.get('codigo')
+		ISBN = request.data.get('ISBN')
+		titulo = request.data.get('titulo')
+		autor = request.data.get('autor')
+		ano = request.data.get('ano')
+		editora = request.data.get('editora')
+
+		livro = models.Livro.objects.create(codigo = codigo, ISBN = ISBN, titulo = titulo, 
+			                                         autor = autor, ano = ano, editora = editora)
+
+		serializer = serializers.LivroSerializer(livro)
+		return Response(serializer.data)
+
+
 
